c685d72dcc88884cf56cf0c7c8794f270a784b2a
Brad K. Harms <brad.k.harms@gmail.com>
2020-05-09 19:44:34 -0600

    update main

--- c/main.c
+++ c/main.c
@@ -1,17 +1,11 @@
 #include <stdlib.h>;
 
-#include "app.h";
-// #include "components/factory.h";
-// #include "components/input.h";
-// #include "components/update.h";
-// #inclide "components/render.h";
+#include "pmem.h"
+#include "app.h"
 
 int main (void) {
-    app_AppData *const appData = app_create(5);
-    // app_registerEngine(factory_engine);
-    // app_registerEngine(input_engine);
-    // app_registerEngine(update_engine);
-    // app_registerEngine(render_engine);
+    pmem_init();
+    app_create(32);
 
-    app_start(appData);
+    app_start();
 }
