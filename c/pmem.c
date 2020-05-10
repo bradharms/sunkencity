9f18a13c57a226d27259bca43dde8204b1d72f5d
Brad K. Harms <brad.k.harms@gmail.com>
2020-05-09 19:22:26 -0600

    convert the private heap docs to a real implementation

---
+++ c/pmem.c
@@ -0,0 +1,57 @@
+#include "pmem.h";
+
+pmem_index_t pmem_allocations[PMEM_CLUSTER_COUNT];
+pmem_cluster_t pmem_clusters[PMEM_CLUSTER_COUNT];
+
+void pmem_init() {
+    for (pmem_index_t cIndex = 0; cIndex < PMEM_CLUSTER_COUNT; cIndex++) {
+        pmem_allocations[cIndex] = 0;
+    }
+}
+
+pmem_cluster_t* pmem_alloc(const pmem_size_t byteCount) {
+    // Determine the number of clusters we need based on the byte count
+    const pmem_index_t cCount =
+        // This is integer division. The output is rounded down.
+        (byteCount / PMEM_CLUSTER_SIZE) +
+        // If the modulus is non-zero, we need one extra cluster.
+        (byteCount % PMEM_CLUSTER_SIZE ? 1 : 0);
+    // If the required cluster count is 0 or is larger than the total number of
+    // clusters available, we can't allocate anything.
+    if (cCount == 0 || cCount >= PMEM_CLUSTER_SIZE) {
+        return (pmem_cluster_t*) 0;
+    }
+    // Search for a fragment of the required length
+    pmem_index_t fIndex = 0;
+    pmem_index_t cIndex = 0;
+    pmem_index_t fLength = 0;
+    pmem_index_t uLength = 0;
+    // Keep scanning until we reach the end of the cluster pool
+    while (cIndex < PMEM_CLUSTER_COUNT) {
+        // Check for an allocation
+        fLength = pmem_allocations[cIndex];
+        // If there is an allocation, skip past it
+        if (fLength != 0) {
+            cIndex = fIndex = fIndex + fLength;
+            uLength = 0;
+            continue;
+        }
+        // There is at least one more unallocated cluster available in this
+        // unallocated region.
+        uLength++;
+        // We have found the required number of clusters.
+        // Write the fragment length into the allocation map to allocate a
+        // new fragment, then return the first cluster in the fragment.
+        if (uLength == cCount) {
+            pmem_allocations[fIndex] = uLength;
+            return pmem_clusters + fIndex;
+        }
+        cIndex++;
+    }
+    // No contiguous region was found to be long enough for the request.
+    return (pmem_cluster_t*) 0;
+}
+
+void pmem_free(pmem_cluster_t* const cluster) {
+    pmem_allocations[cluster - pmem_clusters] = 0;
+}
---
+++ c/pmem.h
@@ -0,0 +1,66 @@
+/**
+ * Private Memory Manager
+ */
+#ifndef PMEM_H
+#define PMEM_H
+
+/**
+ * Number of bytes in a cluster
+ */
+#define PMEM_CLUSTER_SIZE 64
+
+/**
+ * Number of clusters available
+ */
+#define PMEM_CLUSTER_COUNT 255
+
+/**
+ * Type of a single byte
+ */
+typedef unsigned char pmem_byte_t;
+
+/**
+ * Type of a cluster
+ */
+typedef struct { pmem_byte_t b[PMEM_CLUSTER_SIZE]; } pmem_cluster_t;
+
+/**
+ * Type of an index to a cluster
+ */
+typedef unsigned char pmem_index_t;
+
+/**
+ * Type of a size in bytes
+ */
+typedef unsigned int pmem_size_t;
+
+/**
+ * Initialize the heap
+ */
+void pmem_init();
+
+/**
+ * Allocate a number of clusters required for the given number of bytes.
+ * 
+ * Allocations and deallocations are generally random. There is no reliable way
+ * to determine any relationship between the order in which fragments are
+ * allocated and the order in which they are deallocated. Therefore, the only
+ * optimizations we will make will be based on where we know allocated fragments
+ * to be located within the heap and not the order of allocation.
+ * 
+ * This function simply scans the allocation map from the beginning to find a
+ * sequence of clusters equal to the number requested, then marks the location
+ * of the beginning of the sequence so that future allocations will know how far
+ * to skip past it.
+ */
+pmem_cluster_t* pmem_alloc(const pmem_size_t byteCount);
+
+/**
+ * Deallocates a fragment.
+ * 
+ * Deallocating a fragment simply means marking the first cluster in the
+ * allocation map as having a fragment length of 0.
+ */
+void pmem_free(pmem_cluster_t* const cluster);
+
+#endif // PMEM_H
